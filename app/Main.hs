86b8396ff7917c092846d4df16f6c9a5ed599373
Chewie <chwthewke@gmail.com>
2016-06-11 01:37:34 +0200

    [horbits2] Minimal GTK window

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import Control.Monad.IO.Class
+import Graphics.UI.Gtk
+
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  _ <- initGUI
+  window <- windowNew
+  _ <- set window [ windowTitle := mainWindowTitle ]
+  widgetShowAll window
+  _ <- window `on` objectDestroy $ mainQuit
+  mainGUI
+
