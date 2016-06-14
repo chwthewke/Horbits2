a4b176059f76c5c2d675dd3503c530b9fa0dddea
Chewie <chwthewke@gmail.com>
2016-06-14 17:27:17 +0200

    [horbits2] Solved the mystery of unQuantity

--- src/Horbits/Dimensional/Prelude.hs
+++ src/Horbits/Dimensional/Prelude.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
-module Horbits.Dimensional.Prelude(zero, (^*~)) where
+module Horbits.Dimensional.Prelude(zero, (^*~), unQuantity) where
 
 import qualified Prelude as P
 import qualified Linear as L
 import qualified Numeric.Units.Dimensional as D
+import qualified Numeric.Units.Dimensional.Coercion as D
+
+
+unQuantity :: D.Quantity d a -> a
+unQuantity = D.coerce
 
 zero :: (L.Additive f, P.Num a) => D.Quantity d (f a)
-zero = D.dmap (P.const L.zero) D._0
+zero = D.Quantity L.zero
 
 (^*~) :: (L.Additive f, P.Num a) => f a -> D.Unit m d a -> D.Quantity d (f a)
-v ^*~ u = P.undefined
-
-unitApprox :: (P.Num a) => D.Unit m d a -> a
-unitApprox = P.undefined
+v ^*~ u = D.Quantity P.$ P.fmap (s P.*) v
+  where s = unQuantity P.$ 1 D.*~ u
