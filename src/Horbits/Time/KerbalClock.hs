a3132ef1893db3eaed6c74ff3ef72ad358fc5271
Chewie <chwthewke@gmail.com>
2016-06-22 01:15:04 +0200

    Time done

--- src/Horbits/Time/KerbalClock.hs
+++ src/Horbits/Time/KerbalClock.hs
@@ -1,11 +1,13 @@
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell   #-}
 
-module Horbits.Time.KerbalClock where
+module Horbits.Time.KerbalClock(KerbalClock(..), readClock, stopClock, startClock) where
 
 import           Control.Lens                      hiding ((*~))
 -- TODO include SIUnits in Horbits.Dimensional.Prelude, others?
-import           Numeric.Units.Dimensional.SIUnits (second)
+import           Numeric.Units.Dimensional.SIUnits (nano, second)
+import qualified Prelude                           as P
 import           System.Clock
 
 import           Horbits.Dimensional.Prelude
@@ -13,7 +15,7 @@ import           Horbits.Time.KerbalDateTime
 
 
 data TimeFunction = TimeFunction { _timeFunctionBase  :: KerbalInstant
-                                 , _timeFunctionRate  :: Double
+                                 , _timeFunctionRate  :: Dimensionless Double
                                  , _timeFunctionStart :: TimeSpec
                                  } deriving (Show, Eq)
 
@@ -24,6 +26,22 @@ data KerbalClock = StoppedClock KerbalInstant
 makePrisms ''KerbalClock
 
 runTimeFunction :: TimeFunction -> TimeSpec -> KerbalInstant
-runTimeFunction (TimeFunction base rate start) now = base .+^ KerbalTime ((rate * elapsed start now) *~ second)
-  where elapsed (TimeSpec s1 ns1) (TimeSpec s2 ns2) = fromIntegral (s2 - s1) + 1e-9 * fromIntegral (ns2 - ns1)
+runTimeFunction (TimeFunction base rate start) now = base .+^ KerbalTime (rate * (elapsed start now))
 
+elapsed :: TimeSpec -> TimeSpec -> Time Double
+elapsed (TimeSpec s1 ns1) (TimeSpec s2 ns2) =
+    deltaS + deltaNs
+  where
+    deltaS = fromIntegral (s2 P.- s1) *~ second
+    deltaNs = fromIntegral (ns2 P.- ns1) *~ nano second
+
+readClock :: KerbalClock -> TimeSpec -> KerbalInstant
+readClock (StoppedClock i) = const i
+readClock (RunningClock tf) = runTimeFunction tf
+
+stopClock :: KerbalClock -> TimeSpec -> KerbalClock
+stopClock (RunningClock tf) = StoppedClock . runTimeFunction tf
+stopClock s = const s
+
+startClock :: KerbalClock -> Dimensionless Double -> TimeSpec -> KerbalClock
+startClock clock rate now = RunningClock $ TimeFunction (readClock clock now) rate now
